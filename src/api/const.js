8984e603bff858709ad647a19a81b646e65bfe64
Lotlin <lotlin22@gmail.com>
2024-11-27 21:26:48 +0100

    fix finish

--- src/api/const.js
+++ src/api/const.js
@@ -7,7 +7,5 @@ export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const CLIENT_ID = 'wQuzAP3QGIONUsn7Q0uE2g';
 export const RESPONSE_TYPE = 'token';
 export const RANDOM_STRING = 'random-string';
-// export const REDIRECT_URI = 'https://blogged-oa2m5c8jw-lotlins-projects.vercel.app/auth';
-export const REDIRECT_URI = 'http://localhost:3000/auth';
+export const REDIRECT_URI = 'https://blogged-oa2m5c8jw-lotlins-projects.vercel.app/auth';
 export const SCOPE = 'identity read submit';
-// https://blogged-oa2m5c8jw-lotlins-projects.vercel.app/auth
